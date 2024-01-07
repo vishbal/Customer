e0d6f958a282e3966d713f3577859a40ed32224e
Balaji <balaji.vishnubhotla@scadea.com>
2024-01-07 16:56:56 -0600

    Scaffloding of solution items and folders;

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomerApi.Services;
+using CustomerApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public CustomerController()
+        {
+            if (CustomerService.Customers.Count() == 0)
+                CustomerService.CreateCustomers();
+        }
+
+
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return CustomerService.Customers;
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public Customer GetCustomer(int id)
+        {
+             return CustomerService.GetById(id);
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public Customer AddCustomer([FromBody] Customer customer)
+        {
+            return CustomerService.AddCustomer(customer);
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut()]
+        public Customer UpdateCustomer([FromBody] Customer customer)
+        {
+            return CustomerService.UpdateCustomer(customer);
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete()]
+        public bool Delete([FromBody] Customer customer)
+        {
+            return CustomerService.DeleteCustomer(customer);
+        }
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,90 @@
+﻿using CustomerApi.Models;
+
+namespace CustomerApi.Services
+{
+    public static class CustomerData
+    {
+        public static List<Customer> Customers = new List<Customer>();
+
+
+        public static void CreateCustomers()
+        {
+            ClearCustomers();
+            for (int i = 1; i <= 20; i++)
+            {
+                Customers.Add(new Customer
+                {
+                    Id = i,
+                    FirstName = "Customer First Name - " + i,
+                    LastName = "Customer Last Name - " + i,
+                    CreatedDate = new DateTime(),
+                    UpdatedDate = new DateTime(),
+                    Email = "customer" + i + "@example.com"
+                });
+            }
+        }
+        public static void ClearCustomers()
+        {
+            Customers.Clear();
+        }
+
+        public static bool Exists(Customer customer)
+        {
+            return Customers.Any(x => x.Email == customer.Email);
+        }
+
+        public static Customer GetById(int id)
+        {
+            var customer = new Customer();
+            if (Customers.Any(c => c.Id == id))
+                customer = Customers.Find(c => c.Id == id);
+
+            return customer;
+        }
+
+
+        public static Customer AddCustomer(Customer newCustomer)
+        {
+            if (!Exists(newCustomer))
+            {
+                newCustomer.Id = Customers.Max(c => c.Id) + 1;
+                Customers.Add(newCustomer);
+            }
+            else
+            {
+                newCustomer = Customers.First(c => c.Email == newCustomer.Email);
+            }
+            return newCustomer;
+        }
+
+        public static Customer UpdateCustomer(Customer customer)
+        {
+            var existingCustomer = GetById(customer.Id);
+            var index = Customers.IndexOf(existingCustomer);
+            if (index > -1)
+            {
+                Customers[index] = customer;
+                return Customers[index];
+            }
+            else
+            {
+                customer.Id = 0;
+            }
+            return customer;
+        }
+
+        public static bool DeleteCustomer(Customer customer)
+        {
+            if (Exists(customer))
+            {
+                var index = Customers.IndexOf(customer);
+                Customers.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+
+}
